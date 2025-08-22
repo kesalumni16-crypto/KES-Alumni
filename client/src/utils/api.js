27d0d7c2b0f80a958b69d2c4724c940125fe3108
kesalumni16-crypto <kesalumni16@gmail.com>
2025-08-22 07:00:28 +0530

    Updated api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://kes-alumni-bhz1.vercel.app/api';
 
 // Create axios instance
 const api = axios.create({
