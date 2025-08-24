33bb237c5497daff51811349072ba3693ff8c360
kesalumni16-crypto <kesalumni16@gmail.com>
2025-08-24 12:59:07 +0000

    update schema

---
+++ server/prisma/migrations/20250824115656_update_regi/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "public"."Alumni" ADD COLUMN     "city" TEXT,
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "countryCode" TEXT,
+ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT,
+ADD COLUMN     "pincode" TEXT,
+ADD COLUMN     "state" TEXT,
+ADD COLUMN     "street" TEXT;
